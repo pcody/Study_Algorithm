570c3bd99fd0932b4173b353420bd7414c28505a
kiwanpark <gioan92@naver.com>
2021-07-31 16:53:04 +0900

    프로그래머스 해시 전화번호목록 풀이 업로드.. 다시 풀어봐야함

--- Problems/0717_해시_전화번호목록.py
+++ Problems/0717_해시_전화번호목록.py
@@ -1,23 +1,69 @@
 ## 시간초과 3,4번.. 눈물나네
 
+# def solution(phone_book):
+#     answer = True
+#     N = len(phone_book)
+#     #phone_book = list(zip(list(map(len, phone_book)),phone_book))
+#     #phone_book.sort(key = lambda x : (x[0], x[1]))
+#     #_, phone_book = zip(*phone_book)
+#     phone_book.sort()
+    
+#     for j in range(N):
+#         M = len(phone_book[j])
+#         for i in range(N):
+#             O = len(phone_book[i])
+#             if j == i or phone_book[i][0] != phone_book[j][0] or M > O:
+#                 continue
+#             else:
+#                 if phone_book[j] in phone_book[i][:M]:
+#                     return False
+                    
+#     return answer
+
+# def solution(phone_book):
+#     answer = True
+#     N = len(phone_book)
+#     phone_book.sort()
+    
+#     # print(phone_book)
+#     for j in range(N):
+#         M = len(phone_book[j])
+#         for i in range(j+1,N):
+#             O = len(phone_book[i])
+#             # if j == i or phone_book[i][0] != phone_book[j][0] or M > O:
+#             #     continue
+#             # else:
+#                 # if phone_book[j] == phone_book[i][:M]:
+#                 #     return False
+#             chk = 1
+#             for k in range(min(M, O)):
+#                 if phone_book[j][k] != phone_book[i][k]:
+#                     chk = 0
+#                     break
+#             if chk == 1:
+#                 return False
+#     return answer
+
+# 복습하고 업로드하자
 def solution(phone_book):
     answer = True
     N = len(phone_book)
-    phone_book = list(zip(list(map(len, phone_book)),phone_book))
-    phone_book.sort(key = lambda x : (x[0], x[1]))
-    #print(phone_book)
-    _, phone_book = zip(*phone_book)
+    phone_book.sort()
     
-    for j in range(N):
+    # print(phone_book)
+    for j in range(N-1):
         M = len(phone_book[j])
-        for i in range(N):
-            O = len(phone_book[i])
-            if j == i or phone_book[i][0] != phone_book[j][0] or M > O:
-                continue
-            else:
-                if phone_book[j] in phone_book[i][:M]:
-                    return False
-                    
+        # for i in range(j+1,N):
+        O = len(phone_book[j+1])
+        chk = 1
+        for k in range(min(M, O)):
+            if phone_book[j][k] != phone_book[j+1][k]:
+                chk = 0
+                break
+        if chk == 1:
+            return False
+        # else:
+        #     break
     return answer
 
 print(solution(["119", "97674223", "1195524421"]), "False")
