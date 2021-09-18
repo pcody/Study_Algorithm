4d1e8ab3ab86c1f65cd98971ba1555981ee77c72
kiwanpark <gioan92@naver.com>
2021-09-18 16:13:11 +0900

    백준 자바 단계별 학습 시작

---
+++ Problems/_0918_cat.java
@@ -0,0 +1,10 @@
+package JavaCompleted;
+
+public class _0918_cat {
+    public static void main(String[] args) {
+        System.out.println("\\    /\\");
+        System.out.println(" )  ( ')");
+        System.out.println("(  /  )");
+        System.out.println(" \\(__)|");
+    }
+}
---
+++ Problems/_0918_welovekriii.java
@@ -0,0 +1,8 @@
+package JavaCompleted;
+
+public class _0918_welovekriii {
+    public static void main(String[] args) {
+        System.out.println("강한친구 대한육군");
+        System.out.println("강한친구 대한육군");
+    }
+}
